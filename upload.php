e8e3556632b4de56307022e58f6f342cdbbd3f0e
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2023-03-30 15:52:01 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,10 +1,18 @@
 <?php
-  
+
+/**
+ * The objective of the repository is to provide HTML, PHP, and JavaScript code to manage a folder where images are stored..
+ * @author António Lira Fernandes
+ * @version 1.1
+ * @updated 301-03-2023 21:50:00
+ https://github.com/alfZone/imagesFolderNavegation
+ */
+
 if (!empty($_FILES)) {
     //$path=$_REQUEST['pathinfo'];
     $tempFile = $_FILES['file']['tmp_name'];
-    $targetPath = "./";
-    $targetPath = $_REQUEST['pathinfo'];
+    //$targetPath = "./";
+    $targetPath = $_REQUEST['pathinfo'] . "/";
   //echo "ola";
     $targetFile =  $targetPath. $_FILES['file']['name'];
 
